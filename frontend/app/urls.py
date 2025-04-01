8920f1d6f6dc1fcd642e10bdacceda51cf95c0ce
Pablo Marambio Cuzmar (Alumno) <pablo.marambio@usm.cl>
2025-03-31 22:46:24 -0300

    feat: base con error en login

--- frontend/app/urls.py
+++ frontend/app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import *
+from .views import login_view, home, listar_productos, crear_producto, actualizar_producto, entradas_salidas
 
 urlpatterns = [
-    path("login/", login_view, name="login"),  # Login
-    path('home/', home, name='home'),
-    path('actualizar/', actualizar_producto, name='actualizar'),
-    path('entradas_salidas/', entradas_salidas, name='entradas_salidas'),
+    path("login/", login_view, name="login"),
+    path("home/", home, name="home"),
+    
+    # Rutas para los productos
+    path("productos/", listar_productos, name="listar_productos"),
+    path("productos/crear/", crear_producto, name="crear_producto"),
+    path("productos/actualizar/<int:id>/", actualizar_producto, name="actualizar_producto"),
+    path("productos/detalle/<int:id>/", listar_productos, name="detalle_producto"),
+
+    # Rutas para la gestión de inventario
+    path("entradas-salidas/", entradas_salidas, name="entradas_salidas"),
 ]
